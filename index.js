3f3f85ce501bde10de7c03121087f967c2855c98
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2024-04-02 10:44:53 +0530

    create a basic tcp echo server

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-import { prototype } from "events";
 import * as net from "net";
 
 function newCon(socket) {
   console.log("new connection ", socket.remoteAddress, socket.remotePort);
 
-  socket.on("data", (data) => {});
+  socket.on("end", () => {
+    // FIN recieved from the client; collection will be closed automatically
+    console.log("EOF");
+  });
+
+  socket.on("data", (data) => {
+    console.log("data: ", data);
+    // echo back the data again
+    socket.write(data);
+
+    // actively close the connection if the data includes letter 'q'
+    if (data.includes("q")) {
+      console.log("closing the connection");
+      socket.end();
+    }
+  });
 }
 
 // create a RCP server
 let server = net.createServer();
+server.on("error", (err) => {
+  console.log("server error: ", err);
+  server.close();
+});
 server.on("connection", newCon);
 
-server.listen({ host: "127.0.0.1", prot: 99 });
+server.listen({ host: "127.0.0.1", port: 99 });
